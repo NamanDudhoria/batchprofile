d44ff7ed95531cb5d5973671cebc295eadcd97b8
Naman Dudhoria <namandudhoria2002@gmail.com>
2025-02-02 12:23:04 +0530

    settings

--- batchprofile/settings.py
+++ batchprofile/settings.py
@@ -5,7 +5,8 @@ import dj_database_url
 import whitenoise
 
 # Load environment variables from .env file
-load_dotenv()
+if not os.environ.get('RENDER'):
+    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
