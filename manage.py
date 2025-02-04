9e445bae340e97b6405e2c221f66f519e3f0a289
Naman Dudhoria <namandudhoria2002@gmail.com>
2025-02-04 23:45:47 +0530

    manage

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv  # ✅ Import dotenv
 
+# ✅ Load environment variables from .env
+load_dotenv()
 
 def main():
     """Run administrative tasks."""
@@ -16,7 +19,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
