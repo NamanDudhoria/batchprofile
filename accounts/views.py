5b96ec99b345aa21798bb5eb93271fb256b63bc7
Naman Dudhoria <namandudhoria2002@gmail.com>
2025-02-06 11:15:45 +0530

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout  # Added logout
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
-
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'registration/signup.html'
-    success_url = reverse_lazy('login')
 
 def signup_view(request):
     if request.method == 'POST':
@@ -19,7 +14,7 @@ def signup_view(request):
             return redirect('dashboard')
     else:
         form = CustomUserCreationForm()
-    return render(request, 'templates/accounts/signup.html', {'form': form})
+    return render(request, 'accounts/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
@@ -30,13 +25,13 @@ def login_view(request):
             login(request, user)
             return redirect('dashboard')
         else:
-            return render(request, 'templates/accounts/login.html', {'error': 'Invalid username or password'})
+            return render(request, 'accounts/login.html', {'error': 'Invalid username or password'})
     else:
-        return render(request, 'templates/accounts/login.html')
+        return render(request, 'accounts/login.html')
     
 def logout_view(request):
-        logout(request)
-        return redirect('login')
+    logout(request)
+    return redirect('login')
     
 def dashboard_view(request):
-        return render(request, 'templates/profiles/accounts/dashboard.html')
+    return render(request, 'accounts/dashboard.html')
