d325b08bbd30f3bd0afba505a02dd16891e92047
Naman Dudhoria <namandudhoria2002@gmail.com>
2025-02-06 08:01:19 +0530

    signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='forgot_password'),
-    path('signup/', views.SignUpView.as_view(), name='create_account'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
-from profiles.models import CustomUser
-from .forms import CustomUserCreationForm, ProjectForm, ProfileSearchForm   
-from django.views.generic import CreateView
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
-    
+
 def signup_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -21,7 +19,7 @@ def signup_view(request):
             return redirect('dashboard')
     else:
         form = CustomUserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+    return render(request, 'templates/accounts/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
@@ -32,10 +30,13 @@ def login_view(request):
             login(request, user)
             return redirect('dashboard')
         else:
-            return render(request, 'accounts/login.html', {'error': 'Invalid username or password'})
-    return render(request, 'accounts/login.html')
-
-@login_required
+            return render(request, 'templates/accounts/login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'templates/accounts/login.html')
+    
 def logout_view(request):
-    logout(request)
-    return redirect('home')
+        logout(request)
+        return redirect('login')
+    
+def dashboard_view(request):
+        return render(request, 'templates/profiles/accounts/dashboard.html')
