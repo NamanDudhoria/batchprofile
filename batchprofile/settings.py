f250a6f023655d7624981b000510d8d68dc512d8
Naman Dudhoria <namandudhoria2002@gmail.com>
2025-02-10 00:48:29 +0530

    settings

--- batchprofile/settings.py
+++ batchprofile/settings.py
@@ -7,7 +7,7 @@ import dj_database_url
 load_dotenv()
 
 # Build paths inside the project
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
