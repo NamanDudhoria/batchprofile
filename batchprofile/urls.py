9473f8afe7f41581b28fe0fd460e86ee7faab5a4
Naman Dudhoria <namandudhoria2002@gmail.com>
2025-02-10 00:44:54 +0530

    settings

--- batchprofile/urls.py
+++ batchprofile/urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
