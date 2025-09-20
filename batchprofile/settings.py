6f84c05e8cd7b87ca1f5f3ac6ddab7e102aa1562
Naman16082002 <naman_ecok26@iift.edu>
2025-09-20 11:59:57 +0530

    a

--- batchprofile/settings.py
+++ batchprofile/settings.py
@@ -130,12 +130,12 @@ LOGIN_URL = 'accounts:login'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')  # Can be changed to IIFT's SMTP server
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'naman_ecok26@iift.edu')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'naman_ecok26@iift.edu')
 
 # Security settings
 if not DEBUG:
